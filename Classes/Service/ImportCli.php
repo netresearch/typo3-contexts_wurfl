d5e3bc664315597319f7b11f7c403f94ce506c46
Rico Sonntag <rico.sonntag@netresearch.de>
2013-03-18 09:30:13 +0100

    CLI script to import WURFL from local or remote source

--- Classes/Service/ImportCli.php
+++ Classes/Service/ImportCli.php
@@ -30,6 +30,11 @@ class Tx_Contexts_Wurfl_Service_ImportCli extends t3lib_cli
 	const CLI_OK = 0;
 
 	/**
+	 * @var CLI return code: Error on given command.
+	 */
+	const CLI_ERROR_COMMAND = 1;
+
+	/**
 	 * CLI return code: Error.
 	 *
 	 * @var integer
@@ -53,11 +58,17 @@ class Tx_Contexts_Wurfl_Service_ImportCli extends t3lib_cli
 
 		$this->cli_help['description']
 			= 'Import WURFL resource file.' . "\n"
-				. 'import - Imports the WURFL resource file, updating the database';
+				. 'import - Imports the WURFL resource file, '
+				. 'either from local or from remote source';
 
 		$this->cli_help['examples']
-			= 'Import WURFL resource file.' . "\n"
-				. '    php typo3/cli_dispatch.phpsh contexts_wurfl import';
+			= 'Import local WURFL resource file.' . "\n"
+				. '    php typo3/cli_dispatch.phpsh contexts_wurfl import --type local';
+
+		$this->cli_options[]
+			= array('--type local|remote', 'Source of resource file.');
+		$this->cli_options[]
+			= array('--force-update', 'Force update');
 
 		$this->cli_help['author'] = 'Rico Sonntag';
 		$this->cli_help['available tasks'] = 'import';
@@ -95,20 +106,134 @@ class Tx_Contexts_Wurfl_Service_ImportCli extends t3lib_cli
 	 */
 	protected function import()
 	{
+		if (!$this->cli_isArg('--type')) {
+			$this->cli_echo('You need to specify the -s option.' . "\n", true);
+			return self::CLI_ERROR_COMMAND;
+		}
+
+		$type = $this->cli_argValue('--type');
+
 		try {
-			$instance = new Tx_Contexts_Wurfl_Api_Model_Import();
-			return $instance->import();
+			if ($type === 'remote') {
+				$this->importRemote($this->cli_isArg('--force-update'));
+			} elseif ($type === 'local') {
+				$this->importLocal();
+			}
 		}
 		catch (Exception $exception) {
 			$this->cli_echo($exception->getMessage() . "\n", true);
 			return self::CLI_ERROR;
 		}
 
-		$this->cli_echo(
-			'Import of WURFL resource file successfully finished.' . "\n"
+		return self::CLI_OK;
+	}
+
+	/**
+	 * Imports a local version of a WURFL resource file.
+	 *
+	 * @return boolean
+	 */
+	protected function importLocal()
+	{
+		$this->cli_echo('Beginning import from local source.' . "\n");
+
+		$import = new Tx_Contexts_Wurfl_Api_Model_Import(
+			TeraWurflUpdater::SOURCE_LOCAL
 		);
 
-		return self::CLI_OK;
+		$this->showStatus(
+			$import->importLocal(),
+			$import->getUpdater()
+		);
+
+		return true;
+	}
+
+	/**
+	 * Imports the WURFL resource file from the specified source
+	 * (local or remote).
+	 *
+	 * @param boolean $force Force update (only valid for type "remote")
+	 *
+	 * @return boolean
+	 */
+	protected function importRemote($force)
+	{
+		$this->cli_echo('Beginning import from remote source.' . "\n");
+
+		$import = new Tx_Contexts_Wurfl_Api_Model_Import(
+			TeraWurflUpdater::SOURCE_REMOTE
+		);
+
+		$result = $import->importRemote($force);
+
+		// No update available, WURFL data is already up to date
+		if ($result === Tx_Contexts_Wurfl_Api_Model_Import::STATUS_NO_UPDATE) {
+			$this->cli_echo(
+				'No update necessary. Your WURFL data is already up to date.'
+				. ' Use --force-update to update anyway.'
+				. "\n"
+			);
+			return true;
+		}
+
+		$this->showStatus($result, $import->getUpdater());
+
+		return true;
+	}
+
+	/**
+	 * Show status of update.
+	 *
+	 * @param boolean          $status  TRUE/FALSE
+	 * @param TeraWurflUpdater $updater WURFL updater instance
+	 *
+	 * @return void
+	 */
+	protected function showStatus($status, TeraWurflUpdater $updater)
+	{
+		if ($status) {
+			$this->cli_echo('Database Update OK' . "\n");
+
+			$this->cli_echo(
+				'WURFL Version: '
+				. $updater->loader->version
+				. ' (' . $updater->loader->last_updated . ')'
+				. "\n"
+			);
+			$this->cli_echo(
+				'WURFL Devices: '
+				. $updater->loader->mainDevices . "\n"
+			);
+			$this->cli_echo(
+				'PATCH New Devices: '
+				. $updater->loader->patchAddedDevices . "\n"
+			);
+			$this->cli_echo(
+				'PATCH Merged Devices: '
+				. $updater->loader->patchMergedDevices . "\n"
+			);
+
+			if (count($updater->loader->errors) > 0) {
+				$this->cli_echo('Errors:' . "\n");
+
+				foreach ($updater->loader->errors as $error) {
+					$this->cli_echo($error . "\n");
+				}
+
+				$this->cli_echo("\n");
+			}
+		} else {
+			$this->cli_echo('ERROR LOADING DATA!' . "\n");
+			$this->cli_echo('Errors:' . "\n");
+			$this->cli_echo("\n");
+
+			foreach ($updater->loader->errors as $error) {
+				$this->cli_echo($error . "\n");
+			}
+
+			$this->cli_echo("\n");
+		}
 	}
 }
 ?>
