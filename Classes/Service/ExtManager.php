4c5ea72f5e73c0a41279380ff0d10732eba60f14
Rico Sonntag <rico.sonntag@netresearch.de>
2013-03-22 14:35:06 +0100

    TYPOC-6: Extensionmanager HTML

--- Classes/Service/ExtManager.php
+++ Classes/Service/ExtManager.php
@@ -26,26 +26,42 @@ class Tx_Contexts_Wurfl_Service_ExtManager
 	/**
 	 * Generates and returns the information message.
 	 *
+	 * @param array               $params      Name and value from ext_conf_template.txt
+	 * @param t3lib_tsStyleConfig $styleConfig Instance of config style editor
+	 *
 	 * @return string HTML code
 	 */
-	public function displayMessage()
-	{
+	public function displayMessage(
+		array $params, t3lib_tsStyleConfig $styleConfig
+	) {
 		return
 <<<HTML
-	<div style="position: absolute; top: 40px; right: 20px; width: 400px;">
-		<div class="typo3-message message-information">
-			<strong>contexts_wurfl actions:</strong>
-			<ul>
-				<li style="margin: 0px;">
-					<a href="/typo3conf/ext/contexts_wurfl/Scripts/ImportDbLocal.php">
-						Update wurfl db from local file</a>
-				</li>
-				<li style="margin: 0px;">
-					<a href="/typo3conf/ext/contexts_wurfl/Scripts/ImportDbRemote.php">
-						Update wurfl db from remote repository (forced)</a>
-				</li>
-			</ul>
-		</div>
+	<style type="text/css">
+		.wurfl-ext-manager ul {
+			list-style: circle;
+			padding-left: 16px;
+		}
+		.wurfl-ext-manager ul li {
+			margin: 0px;
+		}
+		.wurfl-ext-manager ul li a {
+			text-decoration: underline;
+		}
+		.wurfl-ext-manager ul li a:hover {
+			text-decoration: none;
+		}
+	</style>
+	<div class="typo3-message message-information wurfl-ext-manager">
+		<ul>
+			<li>
+				<a href="/typo3conf/ext/contexts_wurfl/Scripts/ImportDbLocal.php">
+					Update wurfl db from local file</a>
+			</li>
+			<li>
+				<a href="/typo3conf/ext/contexts_wurfl/Scripts/ImportDbRemote.php">
+					Update wurfl db from remote repository (forced)</a>
+			</li>
+		</ul>
 	</div>
 HTML;
 	}
