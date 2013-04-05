80a2c24efd81662b7c4b5ce8e26b79ada1f7d168
Rico Sonntag <rico.sonntag@netresearch.de>
2013-04-05 11:25:04 +0200

    TYPOC-8: Flexform für Geräteeigenschaften

--- ext_contexts.php
+++ ext_contexts.php
@@ -14,10 +14,10 @@ defined('TYPO3_MODE') or die('Access denied.');
 
 // Register our context
 Tx_Contexts_Api_Configuration::registerContextType(
-	'wurfl',
-	'Geräteeigenschaften',
-	'Tx_Contexts_Wurfl_Context_Type_Wurfl',
-	'FILE:EXT:contexts_wurfl/Configuration/flexform/Wurfl.xml'
+    'wurfl',
+    'LLL:EXT:contexts_wurfl/Resources/Private/Language/locallang_mod.xml:context.title',
+    'Tx_Contexts_Wurfl_Context_Type_Wurfl',
+    'FILE:EXT:contexts_wurfl/Configuration/flexform/Wurfl.xml'
 );
 
 ?>
